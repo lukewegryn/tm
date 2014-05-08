4d1901d52656c56229ccb1ae2b8942a7be17a73b
Luke Wegryn <lwegryn@vt.edu>
2014-05-08 19:23:12 -0400

    Added Mac 0SX Functiality

--- clearScreen.py
+++ clearScreen.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 
 def clearScreen(Win_Or_Unix):
 	if Win_Or_Unix == 0:
 		os.system("clear")
 	else:
 		os.system("cls")
+def checkOs():
+	if _platform == "linux" or _playform == "linux2" or _platform == "darwin":
+		return 0
+	elif _platform == "win32":
+		return 1
+
 
