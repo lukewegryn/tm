f1804d12685da3a0c277445612662738e96d8d9f
Luke Wegryn <lwegryn@vt.edu>
2014-05-08 20:00:32 -0400

    Change theseus to a * and minotaur to a #

--- movement.py
+++ movement.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+'''movement.py -- Responsible for generating and updating the list
+that controls the map layout and the thesis and minotaur positions'''
 import sys
-'''theseusX = 20 #sets the initial values of theseus and the minotaur
-theseusY = 7
-minotaurX = 22
-minotaurY = 7'''
+
+wall = '0' #this is the char that makes up the walls
+thesChar = '*' #this represents the theseus
+minoChar = '#' #this represents the minotaur (mino"char" get it?)
 
 def displayMap(map): #displays the map and locations to the terminal
     for j in range(0,len(map)):
@@ -21,18 +22,18 @@ def make_list(stream): #imports the stream and puts it into a list
     return myMap
 
 def wall_check(myMap, theseusX, theseusY): #checks if theseus hit a wall
-    if myMap[(theseusX)+((theseusY)*41)] == '0':
+    if myMap[(theseusX)+((theseusY)*41)] == wall:
         return 0  #if theseus has hit a wall return 0
     else :
         return 1
 
 def map_list(myMap,theseusX, theseusY, theseusXprev, theseusYprev, minotaurX, minotaurY): #puts values into the map list
-    if myMap[(theseusX)+((theseusY)*41)] == '0':
+    if myMap[(theseusX)+((theseusY)*41)] == wall:
         theseusX = theseusXprev
         theseusY = theseusYprev
     else :
-        myMap[(theseusX)+((theseusY)*41)] =  '?'
+        myMap[(theseusX)+((theseusY)*41)] =  thesChar
     if theseusX !=theseusXprev or theseusY != theseusYprev:
         myMap[(theseusXprev)+((theseusYprev)*41)] = ' '
-    myMap[(minotaurX)+((minotaurY)*41)] = '$'
+    myMap[(minotaurX)+((minotaurY)*41)] = minoChar
     return ''.join(myMap)
